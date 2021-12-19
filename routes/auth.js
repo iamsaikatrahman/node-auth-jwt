e2bb92de4525ebc6a578d2ab78605cc0ab13ba19
unknown <saikatrahmanofficial@gmail.com>
2021-12-20 01:56:00 +0600

    connecting

---
+++ routes/auth.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const User = require('../model/User');
+
+router.post('/register', async (req, res) => {
+    // res.send("");
+
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+    try{
+        const savedUser = await user.save();
+        res.send(savedUser);
+    }catch(err){
+        res.status(400).send(err);
+    }finally{
+
+    }
+});
+
+module.exports = router;
