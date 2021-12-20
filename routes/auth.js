1ce3eee14097e00e0cb508cc4339e77769cff386
unknown <saikatrahmanofficial@gmail.com>
2021-12-21 02:11:24 +0600

    joi validation addedd

--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,40 @@
 const router = require('express').Router();
 const User = require('../model/User');
 
+//VALIDATION
+const Joi = require('joi');
+
+const schema = Joi.object({
+    name: Joi.string()
+    .min(6)
+    .required(),
+    email: Joi.string()
+    .min(6)
+    .required()
+    .email(), 
+    password: Joi.string()
+    .min(6)
+    .required()
+});
+
 router.post('/register', async (req, res) => {
-    // res.send("");
 
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    });
-    try{
-        const savedUser = await user.save();
-        res.send(savedUser);
-    }catch(err){
-        res.status(400).send(err);
-    }finally{
+    //LETS VALIDATE THE DATA
+    const {error} =  schema.validate(req.body);
+    res.send(error.details[0].message);
 
-    }
+
+    // const user = new User({
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     password: req.body.password
+    // });
+    // try{
+    //     const savedUser = await user.save();
+    //     res.send(savedUser);
+    // }catch(err){
+    //     res.status(400).send(err);
+    // }
 });
 
 module.exports = router;
